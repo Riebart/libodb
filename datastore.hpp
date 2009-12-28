8c571239f0ca894994e0a36371d4df688773eea8
trav <trav@loki>
2009-12-27 22:07:25 -0600

    Some insight into the workings of the datastore that did not pan out.

--- datastore.hpp
+++ datastore.hpp
@@ -1,12 +1,48 @@
 #ifndef DATASTORE_HPP
 #define DATASTORE_HPP
 
-#include <vector>
 #include <deque>
+#include <stdint.h>
 
+#include "datanode.hpp"
 
+#define DEF_STRUCT(X) \
+    struct struct_X { \
+        uint8_t data [X];\
+    }
 
+DEF_STRUCT(5);
 
+template <uint32_t N>
+struct item
+{
+    uint8_t items [N];
+};
+    
+template <typename T>
+class Datastore
+{
+    public:
+        Datastore <T> ();
+        void add_element(void *);
+        void del_element(void *);
+        void * element_at(uint32_t index);
+        uint32_t get_datasize() { return datasize; };
+    private:
+        uint32_t datasize;
+        std::deque<T> store;
+};
 
+template <typename T>
+Datastore<T>::Datastore()
+{
+    datasize=sizeof(T);
+}
+
+template <typename T>
+void Datastore<T>::add_element(void * new_element)
+{
+    store.push_front( *((T *)new_element) );
+}
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,11 +1,51 @@
 #include <stdlib.h>
+#include <sys/timeb.h>
+#include <stdio.h>
 
 #include "datastore.hpp"
 
+#define TEST_SIZE 200000000
 
-int main ()
+void usage()
 {
+    printf("Usage: test <element size> <num elements>");
+    exit(EXIT_SUCCESS);
+}
 
+
+int main (int argc, char ** argv)
+{
+    if (argc < 2)
+        usage();
+    
+    uint64_t element_size;
+    uint64_t test_size;
+    
+    sscanf(argv[0], "%lu", &element_size);
+    sscanf(argv[1], "%lu", &test_size);
+    
+    
+    struct timeb* start = (struct timeb*)malloc(sizeof(struct timeb));
+    struct timeb* end = (struct timeb*)malloc(sizeof(struct timeb));
+    
+    ftime(start);
+    
+    Datastore < struct item<element_size> > my_datastore;
+    
+    uint64_t i;
+    for ( i=0; i<test_size; i++)
+    {
+        my_datastore.add_element((void *)(&i));
+    }
+    
+    ftime(end);
+    
+    double duration = (end->time - start->time) + 0.001 * (end->millitm - start->millitm);
+    
+    printf("Elapsed time: %fs\n", duration);
+    
+    fgetc(stdin);
+    
     return EXIT_SUCCESS;
 
 }
