af0dc5957fca348ac8dc6ad36612095d74659d36
mike <devnull@localhost>
2010-01-15 10:02:23 -0600

    Modified bench.sh and test.sh so that more information is output. The calls are similar to:
    ./test.sh ./indextest ./indextest.cpp 10
    
    This will complie indextest.cpp in the current directory, output a binary to indextest, and run that 10 times. Output is the average time to complete and the spread of the times.

--- Index Tables/bench.sh
+++ Index Tables/bench.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
 s=0
+M=0
+m=100
 
 for (( i=1 ; i<=$2 ; i++ ))
-    do
+do
     echo -n .
-    s=$(echo "$s + $($1)" | bc)
+    t[$i]=$($1)
+    s=$(echo "$s + ${t[$i]}" | bc)
+    
+    c=$(echo "${t[$i]} > $M" | bc)
+    if [ $c -eq 1 ]
+    then
+        M=${t[$i]}
+    fi
+    
+    c=$(echo "${t[$i]} < $m" | bc)
+    if [ $c -eq 1 ]
+    then
+        m=${t[$i]}
+    fi
 done
 
-echo "Total time: $s seconds"
+s=$(echo "scale = 8; $s / $2" | bc)
+d=$(echo "$M - $m" | bc)
+
+echo "Average time per run is $s seconds with a spread of $d seconds"
--- Index Tables/test.sh
+++ Index Tables/test.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-g++ -O2 -o $1 -DBANK -ltcmalloc_minimal $2
+g++ -O2 -o $1 -DBANK -ltcmalloc $2
+echo -n "BANK: "
+. bench.sh $1 $3
 
-for (( j=1 ; j<=$4 ; j++ ))
-do
-    echo -n "BANK: "
-    . bench.sh $1 $3
-done
+g++ -O2 -o $1 -DDATASTORE -ltcmalloc $2
+echo -n "DATASTORE: "
+. bench.sh $1 $3
 
-g++ -O2 -o $1 -DDATASTORE -ltcmalloc_minimal $2
 
-for (( j=1 ; j<=$4 ; j++ ))
-do
-    echo -n "DATASTORE: "
-    . bench.sh $1 $3
-done
-
-g++ -O2 -o $1 -DDEQUE -ltcmalloc_minimal $2
-
-for (( j=1 ; j<=$4 ; j++))
-do
-    echo -n "DEQUE: "
-    . bench.sh $1 $3
-done
+g++ -O2 -o $1 -DDEQUE -ltcmalloc $2
+echo -n "DEQUE: "
+. bench.sh $1 $3
