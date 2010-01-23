d5002d72a9a912b4194020962149abb8973a2bbd
mike <devnull@localhost>
2010-01-23 15:18:49 -0600

    A change to test.sh that links against tcmalloc_minimal instead of tcmalloc.

--- archive/test.sh
+++ archive/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #g++ -O2 -o $1 -DBANK -ltokyocabinet -Wl,-R /usr/local/lib -ltcmalloc $2
-g++ -O2 -o $1 -DBANK -ltcmalloc $2
+g++ -O2 -o $1 -DBANK -ltcmalloc_minimal $2
 echo -n "BANK: "
 . bench.sh $1 $3
 
@@ -8,7 +8,7 @@ echo -n "BANK: "
 # echo -n "TC: "
 # . bench.sh $1 $3
 
-g++ -O2 -o $1 -DDATASTORE -ltcmalloc $2
+g++ -O2 -o $1 -DDATASTORE -ltcmalloc_minimal $2
 echo -n "DATASTORE: "
 . bench.sh $1 $3
 
