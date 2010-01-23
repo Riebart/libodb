1114e29dadc861eaae7b37a326d90988d9732379
trav <trav@loki>
2010-01-23 13:09:58 -0600

    Cleanup on test.cpp

--- test.cpp
+++ test.cpp
@@ -14,6 +14,23 @@ void usage()
 }
 
 
+void datastore_test(uint64_t element_size, uint64_t test_size)
+{
+    Datastore ds;
+    element_size+=8;
+    uint i;
+    for ( i=0; i<test_size; i++)
+    {
+        void * mem = malloc(element_size);
+//         my_cont.push_front(i);
+//         my_cont.insert(i);
+        ds.add_element((struct datanode *)mem);
+    }
+    
+    
+    
+}
+
 int main (int argc, char ** argv)
 {
     if (argc < 2)
@@ -30,21 +47,9 @@ int main (int argc, char ** argv)
     
     printf("Element size: %lu\nTest Size: %lu\n", element_size, test_size);
     
-    Datastore ds;
     
     ftime(&start);
-    
-    element_size+=sizeof(void*);
-    
-    uint i;
-    for ( i=0; i<test_size; i++)
-    {
-        void * mem = malloc(element_size);
-//         my_cont.push_front(i);
-//         my_cont.insert(i);
-        ds.add_element((struct datanode *)mem);
-    }
-    
+    datastore_test(element_size, test_size);
     ftime(&end);
     
     double duration = (end.time - start.time) + 0.001 * (end.millitm - start.millitm);
