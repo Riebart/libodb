608a764feee4455ca67c1a661b049efe2f702df0
trav <trav@loki>
2010-01-27 14:02:13 -0600

    Beginning of doxygen documentation framework
    To use, run doxygen doc/tm-project.cfg in the root project directory/
    
    Then pointer your browser to doc/index.html

--- test.cpp
+++ test.cpp
@@ -1,3 +1,20 @@
+
+/** File for testing framework
+ *
+ * @file test.cpp
+ * This file is used for testing the database framework. Here you will find
+ * methods and examples that use the framework and test it to make sure it
+ * functions properly.
+ *
+ * @Author Travis
+ *
+ * @warning Specifying too many elements for testing can eat up your RAMs
+ */
+
+
+
+*/
+
 #include <stdlib.h>
 #include <sys/timeb.h>
 #include <stdio.h>
@@ -6,17 +23,35 @@
 
 #define TEST_SIZE 10000000
 
+
+/**A sample comparison function
+ *
+ * @ingroup example
+ * @param [in] a A pointer to the first int to be compared against
+ * @param [in] b A poitner to the second int to be compared
+ * @retval 0 The elements are identical
+ * @retval <0 Element pointed at by a is larger than the one pointed at by b
+ * @retval >0 Element pointed at by b is larger than the one pointed at by a
+ */
 int compare(void* a, void* b)
 {
     return (*(int*)b - *(int*)a);
 }
 
+///Prints out the sample usage
 void usage()
 {
     printf("Usage: test <element size> <number of elements> <number of tests> <test type>\n\tWhere: test_type=0 => BANK, test_type=1 => LL\n");
     exit(EXIT_SUCCESS);
 }
 
+/**Function for testing the database
+ *
+ * @param [in] element_size The size of the elements to be inserted
+ * @param [in] test_size The number of elements to be inserted
+ * @param [in] test_type The type of test to be done. As of now, a 0 indicates
+ that the test should run against the BankDS, a 1 against the LinkedListDS
+ */
 void odb_test(uint64_t element_size, uint64_t test_size, uint8_t test_type)
 {
     ODB* odb;
