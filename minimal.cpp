78d12ea774bd5536f6585c2b6997c29f65380df7
mike <devnull@localhost>
2010-01-28 16:16:33 -0600

    Add querying and deleting query results to minimal.cpp.

--- minimal.cpp
+++ minimal.cpp
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "odb.hpp"
 
+bool condition(void* a)
+{
+    return ((*(long*)a) < 0);
+}
+
 int compare(void* a, void* b)
 {
     return (*(int*)b - *(int*)a);
@@ -8,16 +13,21 @@ int compare(void* a, void* b)
 
 int main (int argc, char ** argv)
 {
-    long v, p = 500;
+    long v, p = 50;
 
     ODB odb(new BankDS(sizeof(long)));
-    odb.create_index(Index::RedBlackTree, 0, compare);
+    Index* ind = odb.create_index(Index::LinkedList, 0, compare);
 
     for (long i = 0 ; i < 10000000 ; i++)
     {
         v = (i + ((rand() % (2 * p + 1)) - p));
         odb.add_data(&v);
     }
-
+    
+    ODB* res = ind->query(condition);
+    printf("Query contains %lu items.\n", res->size());
+    
+    delete res;
+    
     return EXIT_SUCCESS;
 }
