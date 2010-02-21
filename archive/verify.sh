dabec4b58ea1ea0551ecdaebd4494c25ab35cb2a
mike <devnull@localhost>
2010-02-21 17:51:57 -0600

    Removed extraneous and redundant output from verify.sh.

--- archive/verify.sh
+++ archive/verify.sh
@@ -7,32 +7,29 @@ else
     N=$1
 fi
 
-echo "Batch 1..."
+echo -n "Batch 1 "
 for (( j=0 ; j<=3 ; j++ ))
 do
     for (( i=0 ; i<=1 ; i++ ))
     do
+        echo -n "."
         i2=$(echo "2*$i" | bc)
-        echo -n "./test -e 8 -t $N -n 10 -T $j -i $i2..."
         res=$(./test -e 8 -t $N -n 10 -T $j -i $i2 2>"$j.$i2")
-        echo "$res" | grep "^:.*$"
     done
 done
 
-echo ""
-echo "Batch 2 (DROP_DUPLICATES)..."
+echo -en "\nBatch 2 (DROP_DUPLICATES) "
 for (( j=0 ; j<=3 ; j++ ))
 do
     for (( i=0 ; i<=1 ; i++ ))
     do
+        echo -n "."
         i2=$(echo "2*$i+1" | bc)
-        echo -n "./test -e 8 -t $N -n 10 -T $j -i $i2..."
         res=$(./test -e 8 -t $N -n 10 -T $j -i $i2 2>"$j.$i2")
-        echo "$res" | grep "^:.*$"
     done
 done
 
-echo -n "Verifying batch 1 (Assuming 0.0 is authoritative)..."
+echo -en "\nVerifying batch 1 (Assuming 0.0 is authoritative)..."
 auth=$(cat "0.0")
 for (( j=0 ; j<=3 ; j++ ))
 do
