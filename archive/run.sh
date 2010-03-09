21395e9a0d98cc8124f8f6e4fd3c18ef4af29dcc
mike <devnull@localhost>
2010-03-09 09:11:06 -0600

    Adjusted run.sh to allow for specifying multiple paths for files.
    Note that it fails for paths with spaces in the path somewhere. See usage/source for the new argument scheme.

--- archive/run.sh
+++ archive/run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 # Arg1: filename
-# Arg2: fully qualified path
-# Arg3: Number of files to process
+# Arg2: Number of files to process
+# Arg3+: fully qualified path
 
-if [ $# -ne 3 ]; then
-    echo "Usage: run.sh <program name> <full path to files> <number of files>"
+if [ $# -le 2 ]; then
+    echo "Usage: run.sh <program name> <number of files> <full path to files>+"
     exit 1
 fi
 
-sedLoc=$(echo -n $2 |  sed 's/\//\\\//g')
-files=$(ls -1 $2 | tr "\n" "\ " | sed 's/\(.*\).$/\1/g' | sed "s/ / $sedLoc/g" | sed "s/\(.*\)$/$sedLoc\1/g")
-echo $1 $3 $files | sh
+files=""
+
+i=1
+for arg in "$@"
+do
+    if [ $i -ge 3 ]; then
+        sedLoc=$(echo -n ${arg} | sed 's/\//\\\//g')
+        files=$(echo $files $(ls -1 ${arg} | tr "\n" "\ " | sed 's/\(.*\).$/\1/g' | sed "s/ / $sedLoc/g" | sed "s/\(.*\)$/$sedLoc\1/g"))
+    fi
+    let "i += 1"
+done
+
+echo $1 $2 $files | sh
